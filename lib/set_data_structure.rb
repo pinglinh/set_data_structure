7e6b560b61816c7d082c7a3526bb1683a66f83b1
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-10 23:42:06 +0000

    Added tests and implemented code

--- lib/set_data_structure.rb
+++ lib/set_data_structure.rb
@@ -1,3 +1,33 @@
 class SetDataStructure
 
+  include Enumerable
+
+  def initialize(list = [])
+    @set_data = list.dup
+  end
+
+  def each
+    @set_data.each { |data| yield(data) }
+  end
+
+  def size
+    @set_data.size
+  end
+
+  def include?(element)
+    @set_data.include?(element)
+  end
+
+  def add(element)
+    if @set_data.include?(element)
+      print "This element is already in the set!"
+    else
+      @set_data += [element]
+    end
+  end
+
+  def delete(element)
+    @set_data -= [element]
+  end
+
 end
--- spec/set_data_structure_spec.rb
+++ spec/set_data_structure_spec.rb
@@ -2,4 +2,43 @@ require_relative "../lib/set_data_structure"
 
 describe SetDataStructure do
 
+  it 'creates a new object of class SetDataStructure' do
+    set = SetDataStructure.new
+    expect(set.class).to eq(SetDataStructure)
+  end
+
+  it 'returns an empty set' do
+    set = SetDataStructure.new([])
+    expect(set.size).to eq(0)
+  end
+
+  it 'returns the number of elements in the set' do
+    set = SetDataStructure.new([1,2,3,4,5])
+    expect(set.size).to eq(5)
+  end
+
+  it 'contains an element' do
+    set = SetDataStructure.new([1,2,3,4,5])
+    expect(set.include?(4)).to eq(true)
+  end
+
+  it 'adds a new member to the set' do
+    set = SetDataStructure.new([1,2,3,4,5])
+    set.add(8)
+    expect(set.include?(8)).to be true
+  end
+
+  it 'does not add an element if the element is already in the set' do
+    set = SetDataStructure.new([1,2,3,4,5])
+    set.add(4)
+    expect(set).to eq("This element is already in the set!")
+  end
+
+  it 'removes an element from the set' do
+    set = SetDataStructure.new([1,2,3,4,5])
+    set.delete(5)
+    expect(set.to_a).to eq([1,2,3,4])
+  end
+
+
 end
